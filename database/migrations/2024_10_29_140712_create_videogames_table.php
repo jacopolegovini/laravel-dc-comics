cc8a8ab811377cfaf3b1640d1a1cbaf9d122563e
jacopolegovini <legovini96@gmail.com>
2024-10-29 15:16:47 +0100

    migration created

---
+++ database/migrations/2024_10_29_140712_create_videogames_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('videogames', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->year('releaseYearJP');
+            $table->year('releaseYearWW');
+            $table->string('console');
+            $table->text('coverImage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('videogames');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\VideogameController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+
+Route::get('/videogame', [VideogameController::class])->name('videogame');
