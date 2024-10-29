856ec01fa309d3f8c96f496d41997cf76e2bcfaa
jacopolegovini <legovini96@gmail.com>
2024-10-29 15:28:19 +0100

    issue with server fixed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\VideogameController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/videogame', [VideogameController::class])->name('videogame');
+Route::get('/videogame', [VideogameController::class, 'index'])->name('videogame');
