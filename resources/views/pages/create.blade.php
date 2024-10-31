f2bffcb629887821e579232163c65d7ea0f739f7
jacopolegovini <legovini96@gmail.com>
2024-10-31 15:34:42 +0100

    manual validation implemented in 'title'

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -21,23 +21,24 @@
             </h1>
             <div class="mb-3">
                 <label for="videogame-title" class="form-label">Titolo:</label>
-                <input type="text" class="form-control" id="videogame-title" name="title">
+                <input type="text" class="form-control" id="videogame-title" name="title" value="{{(strlen(old('title')) >= 3) ? old('title') : ""}}">
+                @dump(old('title'))
             </div>
             <div class="mb-3">
                 <label for="videogame-releaseYearJP" class="form-label">Uscita in Giappone:</label>
-                <input type="text" class="form-control" id="releaseYearJP" name="releaseYearJP">
+                <input type="text" class="form-control" id="releaseYearJP" name="releaseYearJP" value="{{old('releaseYearJP')}}">
             </div>
             <div class="mb-3">
                 <label for="videogame-releaseYearWW" class="form-label">Uscita nel mondo:</label>
-                <input type="text" class="form-control" id="releaseYearWW" name="releaseYearWW">
+                <input type="text" class="form-control" id="releaseYearWW" name="releaseYearWW" value="{{old('releaseYearWW')}}">
             </div>
             <div class="mb-3">
                 <label for="videogame-console" class="form-label">Console:</label>
-                <input type="text" class="form-control" id="videogame-console" name="console">
+                <input type="text" class="form-control" id="videogame-console" name="console" value="{{old('console')}}">
             </div>
             <div class="mb-3">
                 <label for="videogame-coverImage" class="form-label">Url image cover:</label>
-                <input type="text" class="form-control" id="videogame-coverImage" name="coverImage">
+                <input type="text" class="form-control" id="videogame-coverImage" name="coverImage" value="{{old('coverImage')}}">
             </div>
 
             <div class="mb-3 d-flex justify-content-center align-items-center">
